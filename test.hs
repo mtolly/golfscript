2eb58a84a5f1a2367e6d13ded07dcd8736f7fc73
Michael Tolly <tolly@wisc.edu>
2013-04-23 17:25:27 -0500

    reworked test suite for HUnit

--- test.hs
+++ test.hs
@@ -1,13 +1,14 @@
 module Main where
 
 import Paths_golfscript (getDataFileName)
-import System.FilePath (takeDirectory, replaceDirectory, takeFileName)
-import System.Directory (getDirectoryContents, doesFileExist)
-import Data.Maybe (fromMaybe)
-import Control.Monad (forM_, when)
+import System.FilePath (takeDirectory)
+import System.Directory (getDirectoryContents, doesFileExist, setCurrentDirectory)
+import Data.Maybe (mapMaybe)
+import Control.Monad (forM, (>=>))
 import Data.List (stripPrefix)
 import System.Process (readProcess)
-import Data.IORef
+
+import Test.HUnit
 
 import Language.GolfScript.Base
 import Language.GolfScript.Prelude hiding (rb)
@@ -16,48 +17,44 @@ import Language.GolfScript.Parse
 main :: IO ()
 main = do
   rb <- getDataFileName "test/golfscript.rb"
-  let testDir = takeDirectory rb
-  files <- getDirectoryContents testDir
-  failure <- newIORef False
-  forM_ files $ \f -> fromMaybe (return ()) $ do
-    g <- stripPrefix "test" f
-    h <- stripPrefix "gorp." $ reverse g
-    let j = "test" ++ reverse h
-        fprog = replaceDirectory f testDir
-        fin = replaceDirectory (j ++ ".in") testDir
-    Just $ do
-      b <- doesFileExist fin
-      input <- if b then readFile fin else return ""
-      rubyOut <- runRuby rb fprog input
-      haskellOut <- runHaskell fprog input
-      case haskellOut of
-        Left herr -> do
-          putStrLn herr
-          writeIORef failure True
-        Right hout -> if rubyOut == hout
-          then putStrLn $ "PASSED: Test " ++ f
-          else mapM_ putStrLn
-            [ "FAILED: Test " ++ f
-            , "<Ruby output>"
-            , rubyOut
-            , "<Haskell output>"
-            , hout
-            ] >> writeIORef failure True
-  failed <- readIORef failure
-  when failed $ error "At least one test failed."
+  setCurrentDirectory $ takeDirectory rb
+  tests <- getTests
+  result <- runTestTT $ TestList $ map (uncurry makeTest) tests
+  if errors result + failures result /= 0
+    then error "At least 1 test failed."
+    else return ()
 
-runRuby :: FilePath -> FilePath -> String -> IO String
-runRuby rb fprog input = readProcess "ruby" [rb, fprog] input
+isTest :: FilePath -> Maybe FilePath
+isTest = stripPrefix "test" >=> stripSuffix ".prog" where
+  stripSuffix sfx str = fmap reverse $ stripPrefix (reverse sfx) (reverse str)
 
-runHaskell :: FilePath -> String -> IO (Either String String)
+getTests :: IO [(FilePath, String)]
+getTests = do
+  tests <- fmap (mapMaybe isTest) $ getDirectoryContents "."
+  forM tests $ \tst -> do
+    let program = "test" ++ tst ++ ".prog"
+        input   = "test" ++ tst ++ ".in"
+    b <- doesFileExist input
+    if b then readFile input >>= \i -> return (program, i)
+      else return (program, "")
+
+makeTest :: FilePath -> String -> Test
+makeTest prog input = TestCase $ do
+  rb <- runRuby prog input
+  hs <- runHaskell prog input
+  assertEqual prog rb hs
+
+runRuby :: FilePath -> String -> IO String
+runRuby fprog input = readProcess "ruby" ["golfscript.rb", fprog] input
+
+runHaskell :: FilePath -> String -> IO String
 runHaskell fprog input = do
   p <- fmap eval $ readFile fprog
   let golf = push (Str input) >> runs p >> fmap output stackToArr
   result <- runWrappedIO $ runGolf golf wrappedState
   case result of
-    (Left err, _) -> return $ Left $
-      "FAILED: Test " ++ takeFileName fprog ++ ", Haskell error: " ++ err
-    (Right stk, out) -> return $ Right $ out ++ stk ++ "\n"
+    (Left err,  _)   -> error $ "Haskell error: " ++ err
+    (Right stk, out) -> return $ out ++ stk ++ "\n"
 
 wrappedState :: GolfState WrappedIO
 wrappedState = emptyWith preludeWrappedIO
