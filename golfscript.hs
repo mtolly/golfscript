2af85cf11512839b19c084be29a63ea6c8593264
Michael Tolly <onyxite@gmail.com>
2013-12-03 15:47:00 -0600

    print executable version

--- golfscript.hs
+++ golfscript.hs
@@ -1,17 +1,25 @@
 module Main where
 
+import Data.Version (showVersion)
 import System.Environment (getArgs, getProgName)
-import System.IO (hPutStrLn, stderr)
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr, stdout, Handle)
 
 import Language.GolfScript.Base
 import Language.GolfScript.Parse
 import Language.GolfScript.Prelude
+import Paths_golfscript (version)
 
 main :: IO ()
 main = getArgs >>= \argv -> case argv of
-  [fprog] | fprog /= "-?" -> getContents >>= \c -> readFile fprog >>= go c
-  []                      -> getContents >>= go ""
-  _                       -> printUsage
+  ["-v"]  -> printVersion stdout
+  ["-?"]  -> printUsage stdout
+  [fprog] -> getContents >>= \c -> readFile fprog >>= go c
+  []      -> getContents >>= go ""
+  _       -> do
+    printVersion stderr
+    printUsage stderr
+    exitFailure
 
 go :: String -> String -> IO ()
 go input prog = do
@@ -22,10 +30,18 @@ go input prog = do
     Left  err -> hPutStrLn stderr err
     Right str -> putStrLn str
 
-printUsage :: IO ()
-printUsage = getProgName >>= \pn -> mapM_ (hPutStrLn stderr)
-  [ "GolfScript interpreter in Haskell by Michael Tolly <onyxite@gmail.com>"
-  , "Usage: "++pn++" prog.gs < stdin.txt (stdin to program)"
-  , "       "++pn++" < prog.gs           (empty stdin)"
-  , "       "++pn++" -?                  (print usage)"
+printVersion :: Handle -> IO ()
+printVersion h = getProgName >>= \pn -> mapM_ (hPutStrLn h)
+  [ pn ++ " " ++ showVersion version
+  , "GolfScript language by Darren Smith < http://www.golfscript.com/golfscript >"
+  , "Interpreter in Haskell by Michael Tolly < miketolly@gmail.com >"
   ]
+
+printUsage :: Handle -> IO ()
+printUsage h = getProgName >>= \pn -> mapM_ (hPutStrLn h)
+  [ "Usage: "++pn++" prog.gs < stdin.txt # program with stdin"
+  , "       "++pn++" < prog.gs           # empty stdin"
+  , "       "++pn++" -?                  # print usage"
+  , "       "++pn++" -v                  # print version"
+  ]
+
